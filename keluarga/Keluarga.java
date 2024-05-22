c9ea858fc0b78dc3c3d4fbc4dd0c1f5fea79e46f
unknown <restuahmad612@gmail.com>
2024-05-22 19:58:47 +0700

    lagi

--- keluarga/Keluarga.java
+++ keluarga/Keluarga.java
@@ -1,4 +1,7 @@
-/*
+
+
+
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -14,16 +17,15 @@ public class Keluarga {
      */
     public static void main(String[] args) {
         
-        Manusia bapak = new Orang_Tua("Achmad Esnada Mustaqim", "Magelang", "06 February 1978", "Karyawan Swasta", "Ngluwar,Magelang", "085212190391", 0);
-        Manusia ibu = new Orang_Tua("Nurwati", "Jakarta", "10 December 1983", "Ibu Rumah tangga", "Ngluwar,Magelang", "085879112500", 0);
-        Manusia rizqi = new Anak("Ravanelly Rizqi Pratama", "Jakarta", "07 May 2004", "Pelajar/Mahasiswa", "Ngluwar,Magelang", "081392101010", 0);
-        Manusia aufar = new Anak("Aufar Pradipta Evanizar", "Jakarta", "26 February 2014", "Pelajar", "Ngluwar,Magelang", "0", 0);
+        Manusia bapak = new Orang_Tua("samin hermanto", "bekasi", "24 March 2003", "petani", "bekasi", "085640997282", 0);
+        Manusia ibu = new Orang_Tua("Laelatul Nikmah", "Magelang", "24 March 2003", "Ibu Rumah tangga", "bekasi", "085640997282", 0);
+        Manusia anak = new Anak("Restu ahmad byhaqi", "Magelang", "24 March 2003", "Mahasiswa", "Windusari,Magelang", "085640997282", 0);
        
         System.out.println(bapak.getDetails());
         System.out.println(ibu.getDetails());
-        System.out.println(rizqi.getDetails());
-        System.out.println(aufar.getDetails());
+        System.out.println(anak.getDetails());
     }
     
        
     }
+
